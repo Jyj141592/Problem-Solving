65924ddadbebb61cc132e2fd73fc3f0d3b65ac78
Jyj141592 <140074412+Jyj141592@users.noreply.github.com>
2023-12-25 21:46:44 +0900

    Add files via upload

---
+++ Algorithmic Problem Solving Strategies/8.LIS.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <limits.h>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int n;
+int series[500];
+int cnt[500];
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	scanf("%d", &test_case);
+	for (int T = 0; T < test_case; T++) {
+		scanf("%d", &n);
+		int maximum = 0;
+		for (int i = 0; i < n; i++) {
+			scanf("%d", &series[i]);
+		}
+		for (int i = n - 1; i >= 0; i--) {
+			int maxCnt = 0;
+			for (int j = i + 1; j < n; j++) {
+				if (series[i] < series[j]) {
+					maxCnt = max(maxCnt, cnt[j]);
+				}
+			}
+			cnt[i] = maxCnt + 1;
+			maximum = max(maximum, cnt[i]);
+		}
+		printf("%d\n", maximum);
+	}
+}
