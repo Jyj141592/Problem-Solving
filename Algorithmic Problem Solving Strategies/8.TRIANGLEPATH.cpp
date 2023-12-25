aa6f9c33ee45ace8de678168a3799d4db42148da
Jyj141592 <140074412+Jyj141592@users.noreply.github.com>
2023-12-25 21:14:36 +0900

    Add files via upload

---
+++ Algorithmic Problem Solving Strategies/8.TRIANGLEPATH.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <limits.h>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int n;
+int board[100][100];
+int maxSum[100][100];
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	scanf("%d", &test_case);
+	for (int T = 0; T < test_case; T++) {
+		scanf("%d", &n);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				scanf("%d", &board[i][j]);
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			maxSum[n - 1][i] = board[n - 1][i];
+		}
+		for (int i = n - 2; i >= 0; i--) {
+			for (int j = 0; j <= i; j++) {
+				maxSum[i][j] = max(maxSum[i + 1][j], maxSum[i + 1][j + 1]) + board[i][j];
+			}
+		}
+		printf("%d\n", maxSum[0][0]);
+	}
+}
