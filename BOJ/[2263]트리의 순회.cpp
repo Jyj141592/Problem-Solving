5e0f6b80fd320561022517c0edf849fe4143c896
Jyj141592 <140074412+Jyj141592@users.noreply.github.com>
2024-01-10 21:00:18 +0900

    Add files via upload

---
+++ BOJ/[2263]트리의 순회.cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <limits.h>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+#define PUSH(x) pre[cnt++] = (x)
+int n;
+int post[100000];
+int in[100000];
+int pre[100000];
+int inPos[100001];
+int cnt = 0;
+
+void toPreOrder(int iLeft, int pLeft, int count) {
+	if (count <= 1) {
+		if (count == 1) {
+			PUSH(in[iLeft]);
+		}
+		return;
+	}
+	int root = post[pLeft + count - 1];
+	PUSH(root);
+	int left, right;
+	left = inPos[root] - iLeft;
+	right = count - left - 1;
+	toPreOrder(iLeft, pLeft, left);
+	toPreOrder(iLeft + left + 1, pLeft + left, right);
+}
+
+int main(int argc, char** argv)
+{
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &in[i]);
+		inPos[in[i]] = i;
+	}
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &post[i]);
+	}
+	toPreOrder(0, 0, n);
+	for (int i = 0; i < n; i++) {
+		printf("%d ", pre[i]);
+	}
+}
