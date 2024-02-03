4d6a37d913c21b1b19fa7e70e434bc2cf7ba2305
Jyj141592 <140074412+Jyj141592@users.noreply.github.com>
2024-02-04 05:38:58 +0900

    Add files via upload

---
+++ BOJ/[3653]영화 수집.cpp
@@ -0,0 +1,71 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+int n, m;
+int entry;
+int tree[524289];
+int dvd[100001];
+
+int pSum(int s) {
+    if (s >= entry) return 0;
+    int idx = 1;
+    int left = 0, right = entry;
+    while (s != left) {
+        int mid = (left + right) >> 1;
+        if (s >= mid) {
+            left = mid;
+            idx = (idx << 1) + 1;
+        }
+        else {
+            right = mid;
+            idx = idx << 1;
+        }
+
+    }
+    return tree[idx] + pSum(right);
+}
+
+void update(int num, int value) {
+    int idx = entry + num;
+    while(idx > 0) {
+        tree[idx] += value;
+        idx = idx >> 1;
+    }
+}
+
+int main()
+{
+    int tc;
+    scanf("%d", &tc);
+    for (int T = 0; T < tc; T++) {
+        scanf("%d %d", &n, &m);
+        entry = 1;
+        while (entry < m + n) {
+            entry = entry << 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            dvd[i] = n - i;
+        }
+        for (int i = 0; i < entry; i++) {
+            if (i < n) {
+                tree[entry + i] = 1;
+            }
+            else tree[entry + i] = 0;
+        }
+        for (int i = entry - 1; i > 0; i--) {
+            int child = i << 1;
+            tree[i] = tree[child] + tree[child + 1];
+        }
+        for (int i = 0; i < m; i++) {
+            int num;
+            scanf("%d", &num);
+            printf("%d ", pSum(dvd[num] + 1));
+            update(dvd[num], -1);
+            dvd[num] = n + i;
+            update(n + i, 1);
+        }
+        printf("\n");
+    }
+}
---
+++ BOJ/[3830]교수님은 기다리지 않는다.cpp
@@ -0,0 +1,99 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <utility>
+using namespace std;
+#define FINAL -2
+#define NONE -1
+int n, m;
+int group[100001];
+pair<int, int> gParent[100001];
+int g = 0;
+int offset[100001];
+
+pair<int, int> findRoot(int gNum) {
+    if (gParent[gNum].first == FINAL) return { gNum, 0 };
+    pair<int, int> root = findRoot(gParent[gNum].first);
+    gParent[gNum].first = root.first;
+    gParent[gNum].second = root.second + gParent[gNum].second;
+    return gParent[gNum];
+}
+
+void update(int num) {
+    if (group[num] == NONE) return;
+    if (gParent[group[num]].first != FINAL) {
+        pair<int, int> root = findRoot(group[num]);
+        offset[num] += root.second;
+        group[num] = root.first;
+    }
+}
+
+int main()
+{
+    scanf("%d %d", &n, &m);
+    while (n != 0 && m != 0) {
+        for (int i = 1; i <= n; i++) {
+            group[i] = NONE;
+        }
+        g = 0;
+        for (int i = 0; i < m; i++) {
+            char c = getchar();
+            while (c != '!' && c != '?') {
+                c = getchar();
+            }
+            if (c == '!') {
+                int a, b, w;
+                scanf("%d %d %d", &a, &b, &w);
+                if (group[a] != NONE) {
+                    if (group[b] != NONE) {
+                        update(a);
+                        update(b);
+                        if (group[a] != group[b]) {
+                            int temp = offset[b] - w;
+                            gParent[group[a]].first = group[b];
+                            gParent[group[a]].second = temp - offset[a];
+                            group[a] = group[b];
+                            offset[a] = temp;
+                        }
+                    }
+                    else {
+                        update(a);
+                        group[b] = group[a];
+                        offset[b] = offset[a] + w;
+                    }
+                }
+                else {
+                    if (group[b] != NONE) {
+                        update(b);
+                        group[a] = group[b];
+                        offset[a] = offset[b] - w;
+                    }
+                    else {
+                        gParent[g] = { FINAL, FINAL };
+                        group[a] = group[b] = g;
+                        g++;
+                        offset[a] = 0;
+                        offset[b] = w;
+                    }
+                }
+            }
+            else {
+                int a, b;
+                scanf("%d %d", &a, &b);
+                update(a);
+                update(b);
+                if (group[a] == NONE || group[b] == NONE) {
+                    printf("UNKNOWN\n");
+                }
+                else if (group[a] != group[b]) {
+                    printf("UNKNOWN\n");
+                }
+                else {
+                    printf("%d\n", offset[b] - offset[a]);
+                }
+            }
+        }
+        scanf("%d %d", &n, &m);
+    }
+}
