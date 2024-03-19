f017c10d2175ee8eaf3a06ff0e7f61b19c3d89c3
Jyj141592 <140074412+Jyj141592@users.noreply.github.com>
2024-03-20 00:43:04 +0900

    Add files via upload

---
+++ BOJ/[12850]본대 산책2.cpp
@@ -0,0 +1,70 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+// 1036
+using namespace std;
+#define MOD 1000000007
+int n;
+int m[8][8] = { {1,0,0,0,0,0,0,0},
+    {0,1,0,0,0,0,0,0},
+    {0,0,1,0,0,0,0,0},
+    {0,0,0,1,0,0,0,0},
+    {0,0,0,0,1,0,0,0},
+    {0,0,0,0,0,1,0,0},
+    {0,0,0,0,0,0,1,0},
+    {0,0,0,0,0,0,0,1} };
+int mul[8][8] = {
+    {0,1,1,0,0,0,0,0},
+    {1,0,1,1,0,0,0,0},
+    {1,1,0,1,1,0,0,0},
+    {0,1,1,0,1,1,0,0},
+    {0,0,1,1,0,1,0,1},
+    {0,0,0,1,1,0,1,0},
+    {0,0,0,0,0,1,0,1},
+    {0,0,0,0,1,0,1,0}
+};
+
+int main()
+{
+    scanf("%d", &n);
+    int temp[8][8];
+    while (n > 0) {
+        if (n & 1) {
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    temp[i][j] = m[i][j];
+                }
+            }
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    long long val = 0;
+                    for (int k = 0; k < 8; k++) {
+                        val += (long long)temp[i][k] * mul[k][j];
+                        val = val % MOD;
+                    }
+                    m[i][j] = val;
+                }
+            }
+        }
+        n = n >> 1;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                temp[i][j] = mul[i][j];
+            }
+        }
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                long long val = 0;
+                for (int k = 0; k < 8; k++) {
+                    val += (long long)temp[i][k] * temp[k][j];
+                    val = val % MOD;
+                }
+                mul[i][j] = val;
+            }
+        }
+    }
+    printf("%d", m[0][0]);
+}
---
+++ BOJ/[17401]일하는 세포.cpp
@@ -0,0 +1,102 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+// 1036
+using namespace std;
+#define MOD 1000000007
+int t, n, d;
+int map[100][20][20];
+int mul[20][20];
+int rest[20][20];
+int result[20][20];
+
+void matrix_mul(int dest[20][20], int src[20][20]) {
+    int temp[20][20];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            temp[i][j] = dest[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            long long val = 0;
+            for (int k = 0; k < n; k++) {
+                val += (long long)temp[i][k] * src[k][j];
+                val = val % MOD;
+            }
+            dest[i][j] = val;
+        }
+    }
+}
+
+
+int main()
+{
+    scanf("%d %d %d", &t, &n, &d);
+    for (int i = 0; i < n; i++) {
+        result[i][i] = 1;
+        mul[i][i] = 1;
+    }
+
+    for (int i = 0; i < t; i++) {
+        int e, a, b, c;
+        scanf("%d", &e);
+        for (int j = 0; j < e; j++) {
+            scanf("%d %d %d", &a, &b, &c);
+            map[i][a - 1][b - 1] = c;
+        }
+    }
+    if (t > d) {
+        for (int i = 0; i < d; i++) {
+            matrix_mul(result, map[i]);
+        }
+    }
+    else {
+        int r = d % t;
+        for (int i = 0; i < t; i++) {
+            if (r == i) {
+                for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < n; k++) {
+                        rest[j][k] = mul[j][k];
+                    }
+                }
+            }
+            matrix_mul(mul, map[i]);
+        }
+        int q = d / t;
+        while (q > 0) {
+            if (q & 1) {
+                matrix_mul(result, mul);
+            }
+            int temp[20][20];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    temp[i][j] = mul[i][j];
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    long long val = 0;
+                    for (int k = 0; k < n; k++) {
+                        val += (long long)temp[i][k] * temp[k][j];
+                        val = val % MOD;
+                    }
+                    mul[i][j] = val;
+                }
+            }
+            q = q >> 1;
+        }
+        if (r > 0) {
+            matrix_mul(result, rest);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+}
