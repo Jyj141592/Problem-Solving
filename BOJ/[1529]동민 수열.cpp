fb65dbb898e007dad7f685c3960c34557c5ca54f
Jyj141592 <140074412+Jyj141592@users.noreply.github.com>
2024-03-28 21:07:18 +0900

    Add files via upload

---
+++ BOJ/[1529]동민 수열.cpp
@@ -0,0 +1,95 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+#define MOD 1234567891
+int n, l;
+int num[50];
+// 0 -> 7...7, 1 -> 7...4, 2 -> 4...7, 3 -> 4...4
+int type[4];
+int mul[2][2];
+int result[2][2];
+
+int main()
+{
+    scanf("%d%d", &n, &l);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &num[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        bool pass = false;
+        for (int j = i + 1; j < n; j++) {
+            if (num[j] == num[i]) {
+                pass = true;
+                break;
+            }
+        }
+        if (pass) continue;
+        int t = num[i];
+        int r = t % 10;
+        t = t / 10;
+        int idx = 0;
+        if (r != 4 && r != 7) continue;
+        if (r == 4) idx += 1;
+        while (t > 0) {
+            r = t % 10;
+            if (r != 4 && r != 7) {
+                pass = true;
+                break;
+            }
+            t = t / 10;
+        }
+        if (pass) continue;
+        if (r == 4) idx += 2;
+        type[idx]++;
+    }
+
+    result[0][0] = result[1][1] = 1;
+    mul[0][0] = type[0];
+    mul[0][1] = type[2];
+    mul[1][0] = type[1];
+    mul[1][1] = type[3];
+
+    int temp[2][2];
+    while (l > 0) {
+        if (l & 1) {
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    temp[i][j] = result[i][j];
+                }
+            }
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                    long long t = 0;
+                    for (int k = 0; k < 2; k++) {
+                        t += ((long long)temp[i][k] * mul[k][j]) % MOD;
+                        t = t % MOD;
+                    }
+                    result[i][j] = t;
+                }
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                temp[i][j] = mul[i][j];
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                long long t = 0;
+                for (int k = 0; k < 2; k++) {
+                    t += ((long long)temp[i][k] * temp[k][j]) % MOD;
+                    t = t % MOD;
+                }
+                mul[i][j] = t;
+            }
+        }
+        l = l >> 1;
+    }
+    long long t = result[0][0] + result[0][1] + result[1][0] + result[1][1];
+    printf("%d", t % MOD);
+}
---
+++ BOJ/[15318]새로운 수열.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+int n;
+int a[300000];
+long long b[300000];
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    long long add = 0;
+    int sign = 1;
+    for (int i = 0; i < n; i++) {
+        long long temp = sign * a[i];
+        add += temp;
+        b[0] += temp * (i + 1);
+        sign = -sign;
+    }
+    int prev = (n & 1) ? 1 : -1;
+    for (int i = 1; i < n; i++) {
+        b[i] = -b[i - 1] + add;
+
+        b[i] += a[i - 1] * n * prev;
+        add = -add;
+        if (prev == 1) {
+            add += 2 * a[i - 1];
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        printf("%lld ", b[i]);
+    }
+}
